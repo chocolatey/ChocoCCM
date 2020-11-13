7204d9119464a66de405761fbe5c3258ff96d8e5
Stephen Valdinger <stephen@chocolatey.io>
2020-11-13 15:41:19 -0500

    Updating pester tests

---
+++ src/Tests/computer.tests.ps1
@@ -0,0 +1,60 @@
+$module = (Get-ChildItem "$($env:BuildRepositoryLocalPath)" -Recurse -Filter *.psd1).FullName[1]
+
+Import-Module $module -Force
+
+
+Function Get-CCMComputer {
+
+    param($Computer)
+
+    Invoke-RestMethod -Uri 'https://google.com'
+}
+Describe "CCM Computer Functions" {
+
+    Mock Invoke-Restmethod -MockWith {
+
+        if ($Computer -and $Computer.Count -gt 1) {
+            $Computer | % { [pscustomobject] @{ name = $_ } }
+            
+        }
+        else {
+            $foo = [pscustomobject]@{
+                computerGuid                               = "$((New-Guid).Guid)"                                                               
+                name                                       = 'ccmserver'                                                                                         
+                friendlyName                               = $null
+                ipAddress                                  = 10.0.2.15
+                listLocalOnlyReportChecksum                = - 2031453698
+                outdatedReportChecksum                     = 487
+                lastCheckInDateTime                        = "$(Get-Date)"
+                fqdn                                       = 'ccmserver'
+                ccmServiceName                             = 'ccmserver'
+                availableForDeploymentsBasedOnLicenseCount = True
+                optedIntoDeploymentBasedOnConfig           = True
+                software                                   = {}
+                groups                                     = {}
+                users                                      = {}
+                chocolateyConfigurationFeatures            = {}
+                id                                         = 1
+            } 
+
+            return $foo
+        }
+    }
+
+    It "Returns an object" {
+
+        $computer = Get-CCMComputer
+        $computer | Should -BeOfType "System.Management.Automation.PSCustomObject"
+    }
+
+    It "Should have 16 properties" {
+
+        $computer = Get-CCMComputer -Computer Foo
+        ($computer | Get-Member -MemberType NoteProperty).Count | Should -Be 16
+    }
+
+    It "Returns multiple objects if an array is passed" {
+        $computer = Get-CCMComputer -Computer webserver, dbserver
+        $computer.Count | Should -Be 2
+    }
+}
---
+++ src/Tests/help.tests.ps1
@@ -0,0 +1,19 @@
+#Blocked until Docs PR is merged
+
+<#
+$module = (Get-ChildItem "$($env:BuildRepositoryLocalPath)" -Recurse -Filter *.psd1).FullName[1]
+
+Import-Module $module -Force
+
+$commands = Get-Command -Module ChocoCCM
+
+Describe "Functions have valid help" {
+   
+    foreach ($Command in $Commands) {
+        It "$($Command.Name): HelpUri in CmdletBinding() resolves correctly" {
+            $request = [System.Net.WebRequest]::Create("$($Command.HelpUri)")
+            $request.StatusCode | Should -Be 'OK'
+        }
+    }
+}
+#>
