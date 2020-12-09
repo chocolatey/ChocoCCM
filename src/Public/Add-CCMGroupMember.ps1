bb6f671b9cbd2ef8ce67ece8b3b4adafbf949b94
Stephen Valdinger <stephen@chocolatey.io>
2020-12-08 22:25:44 -0500

    (#39) Use empty arrays in json body if variables are not populated

--- src/Public/Add-CCMGroupMember.ps1
+++ src/Public/Add-CCMGroupMember.ps1
@@ -19,19 +19,19 @@ function Add-CCMGroupMember {
     Add-CCMGroupMember -Group 'Newly Imaged' -Computer Lab1,Lab2,Lab3
     
     #>
-    [cmdletBinding(HelpUri="https://chocolatey.org/docs/add-ccmgroup-member")]
+    [cmdletBinding(HelpUri = "https://chocolatey.org/docs/add-ccmgroup-member")]
     param(
         [parameter(Mandatory = $true)]
         [parameter(ParameterSetName = "Computer")]
         [parameter(ParameterSetName = "Group")]
         [ArgumentCompleter(
             {
-                param($Command,$Parameter,$WordToComplete,$CommandAst,$FakeBoundParams)
+                param($Command, $Parameter, $WordToComplete, $CommandAst, $FakeBoundParams)
                 $r = (Get-CCMGroup).Name
                 
 
-                If($WordToComplete){
-                    $r.Where{$_ -match "^$WordToComplete"}
+                If ($WordToComplete) {
+                    $r.Where{ $_ -match "^$WordToComplete" }
                 }
 
                 Else {
@@ -54,7 +54,7 @@ function Add-CCMGroupMember {
 
     begin {
         
-        if(-not $Session){
+        if (-not $Session) {
             throw "Not authenticated! Please run Connect-CCMServer first!"
         }
         
@@ -64,53 +64,81 @@ function Add-CCMGroupMember {
         $ComputerCollection = [System.Collections.Generic.List[psobject]]::new()
         $GroupCollection = [System.Collections.Generic.List[psobject]]::new()
 
-        $id  = Get-CCMGroup -Group $name | Select-Object -ExpandProperty Id
-        $current = Get-CCMGroup -Id $id| Select-Object computers
-        $current.computers | ForEach-Object { $ComputerCollection.Add($($_.computerId))}
+        $current = Get-CCMGroupMember -Group $Name
+        $current.computers | ForEach-Object { 
+            $obj = [pscustomobject]@{
+                computerId = $_.computerId
+                groupId    = $_.groupId
+                id         = $_.id
+            }
+            $ComputerCollection.Add($obj)
+        }
+
+        $current.Groups | ForEach-Object {
+            $obj  = [pscustomobject]@{
+                subGroupId = $_.groupId
+            }
+            $GroupCollection.Add($obj)
+        }
+
         #$current.id | % {$ComputerCollection.Add($_)}
         
     }
 
     process {
+        foreach ($c in $Computer) {
+            $Cresult = $computers | Where-Object { $_.Name -eq "$c" } | Select-Object -ExpandProperty Id
+            $ComputerCollection.Add($([pscustomobject]@{computerId = $Cresult}))
+        }
 
-        switch($PSCmdlet.ParameterSetName){
+        $processedComputers = @($ComputerCollection)
+
+        foreach ($g in $Group) {
+            $Gresult = $groups | Where-Object { $_.Name -eq "$g" } | Select-Object -ExpandProperty Id
+            $GroupCollection.Add($([psCustomObject]@{subGroupId = $Gresult}))
+        }
+        $processedGroups = @($GroupCollection)
+
+        switch ($PSCmdlet.ParameterSetName) {
             'Computer' {
 
-                foreach ($c in $Computer) {
-                    $Cresult = $computers | Where-Object { $_.Name -eq "$c" } | Select-Object -ExpandProperty Id
-                    $ComputerCollection.Add($Cresult)
-                }
-
-                $processedComputers = @($ComputerCollection | ForEach-Object { [pscustomobject]@{ computerId = "$_"}})
                 
+                $Body = @{
+                    Name        = $Name
+                    Id          = ($groups | Where-Object { $_.name -eq "$Name" } | Select-Object  -ExpandProperty Id)
+                    Description = ($groups | Where-Object { $_.name -eq "$Name" } | Select-Object  -ExpandProperty Description)
+                    Groups      = @($processedGroups)
+                    Computers   = @($processedComputers)
+                } | ConvertTo-Json
+    
             }
 
             'Group' {
 
-                foreach ($g in $Group) {
-                    $Gresult = $groups | Where-Object { $_.Name -eq "$g" } | Select-Object -ExpandProperty Id
-                    $GroupCollection.Add($Gresult)
-                }
-                    $processedGroups = @($GroupCollection | ForEach-Object { [pscustomobject]@{ computerId ="$_" }})
-                }
-            }
+                
+                $Body = @{
+                    Name        = $Name
+                    Id          = ($groups | Where-Object { $_.name -eq "$Name" } | Select-Object  -ExpandProperty Id)
+                    Description = ($groups | Where-Object { $_.name -eq "$Name" } | Select-Object  -ExpandProperty Description)
+                    Groups      = @($processedGroups)
+                    Computers   = @($processedComputers)
+                } | ConvertTo-Json
         
-
+            }
+        }
+        
+        
         $irmParams = @{
             Uri         = "$($protocol)://$hostname/api/services/app/Groups/CreateOrEdit"
             Method      = "post"
             ContentType = "application/json"
-            Body        = @{
-                Name        = $Name
-                Id          = ($groups | Where-Object { $_.name -eq "$Name"} | Select-Object  -ExpandProperty Id)
-                Description = ($groups | Where-Object { $_.name -eq "$Name"} | Select-Object  -ExpandProperty Description)
-                Groups      = $processedGroups
-                Computers   = $processedComputers
-            } | ConvertTo-Json
             WebSession  = $Session
+            Body        = $Body
         }
 
         try {
+
+            Write-Verbose $irmParams.Body
             $null = Invoke-RestMethod @irmParams -ErrorAction Stop
         }
 
