235fc6f905cc1f2ba4030891cdd83f17411d74ef
Stephen Valdinger <stephen@chocolatey.io>
2021-02-10 16:00:40 -0500

    (maint) Add GitVersion logic to build script

--- src/build.ps1
+++ src/build.ps1
@@ -14,7 +14,15 @@ Param(
 
     [Parameter()]
     [Switch]
-    $TestDeploy
+    $TestDeploy,
+
+    [Parameter()]
+    [Switch]
+    $BuildLocal,
+
+    [Parameter()]
+    [Switch]
+    $GitVersion
 )
 
 
@@ -23,6 +31,15 @@ process {
     $root = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
     Switch ($true) {
+        
+        $GitVersion {
+            
+            $dotnetArgs =  @('tool', 'install', 'gitversion.tool', "--version 5.6.6", "--tool-path $PSScriptRoot")
+            & dotnet @dotnetArgs
+
+            $GitVersionTool = (Get-ChildItem $PSScriptRoot -Recurse -Filter "dotnet*gitversion*")
+            Write-Output "##teamcity[GitVersionTool '$GitVersionTool']"
+        }
 
         $Test {
             Install-Module Pester -MaximumVersion 4.99 -SkipPublisherCheck -Force
@@ -57,6 +74,37 @@ if(-not (Test-Path $env:ChocolateyInstall\license\chocolatey.license.xml)){
 
             }
 
+            Write-Host $env:GitVersionTool
+
+        }
+
+        $BuildLocal {
+            If (Test-Path $root\Output) {
+                
+                Remove-Item $root\Output -Recurse -Force
+
+            }
+
+            $null = New-item "$root\Output\ChocoCCM" -ItemType Directory
+
+            Copy-Item "$root\ChocoCCM.psd1" "$root\Output\ChocoCCM"
+
+            $string = @'
+if(-not $IsMacOS){
+    if(-not (Test-Path $env:ChocolateyInstall\license\chocolatey.license.xml)){
+        throw "A licensed version of Chocolatey For Business is required to import and use this module"
+    }
+}
+
+'@
+
+        $string | Add-Content "$root\Output\ChocoCCM\ChocoCCM.psm1"
+
+            Get-ChildItem -Path $root\Public\*.ps1 | Foreach-Object {
+
+                Get-Content $_.FullName | Add-Content "$root\Output\ChocoCCM\ChocoCCM.psm1"
+
+            }
         }
 
         $TestDeploy {
