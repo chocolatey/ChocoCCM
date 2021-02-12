d605a80126fa46e8b0d3fa9b912906d05a8aa767
Stephen Valdinger <stephen@chocolatey.io>
2021-02-12 16:26:04 -0500

    (maint) add back full param block now

--- src/build.ps1
+++ src/build.ps1
@@ -1,115 +1,120 @@
-Param($Step)
+[CmdletBinding()]
+Param(
+    [Parameter()]
+    [ValidateScript('GitVersion', 'Build', 'Test', 'Deploy')]
+    [String]
+    $Step
+)
 
 
-    $root = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$root = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-    Switch ($Step) {
+Switch ($Step) {
         
-        'GitVersion' {
+    'GitVersion' {
 
-            $DotNetChannel = "Current";
-            $DotNetVersion = "2.2.401";
-            $DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
+        $DotNetChannel = "Current";
+        $DotNetVersion = "2.2.401";
+        $DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
 
-            ###########################################################################
-            # INSTALL .NET CORE CLI
-            ###########################################################################
+        ###########################################################################
+        # INSTALL .NET CORE CLI
+        ###########################################################################
 
-            Function Remove-PathVariable([string]$VariableToRemove) {
-                $path = [Environment]::GetEnvironmentVariable("PATH", "User")
-                if ($path -ne $null) {
-                    $newItems = $path.Split(';', [StringSplitOptions]::RemoveEmptyEntries) | Where-Object { "$($_)" -inotlike $VariableToRemove }
-                    [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join(';', $newItems), "User")
-                }
-
-                $path = [Environment]::GetEnvironmentVariable("PATH", "Process")
-                if ($path -ne $null) {
-                    $newItems = $path.Split(';', [StringSplitOptions]::RemoveEmptyEntries) | Where-Object { "$($_)" -inotlike $VariableToRemove }
-                    [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join(';', $newItems), "Process")
-                }
+        Function Remove-PathVariable([string]$VariableToRemove) {
+            $path = [Environment]::GetEnvironmentVariable("PATH", "User")
+            if ($path -ne $null) {
+                $newItems = $path.Split(';', [StringSplitOptions]::RemoveEmptyEntries) | Where-Object { "$($_)" -inotlike $VariableToRemove }
+                [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join(';', $newItems), "User")
             }
 
-            # Get .NET Core CLI path if installed.
-            $FoundDotNetCliVersion = $null;
-            if (Get-Command dotnet -ErrorAction SilentlyContinue) {
-                $FoundDotNetCliVersion = dotnet --version;
+            $path = [Environment]::GetEnvironmentVariable("PATH", "Process")
+            if ($path -ne $null) {
+                $newItems = $path.Split(';', [StringSplitOptions]::RemoveEmptyEntries) | Where-Object { "$($_)" -inotlike $VariableToRemove }
+                [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join(';', $newItems), "Process")
             }
+        }
 
-            if ($FoundDotNetCliVersion -ne $DotNetVersion) {
-                $InstallPath = Join-Path $PSScriptRoot ".dotnet"
-                if (!(Test-Path $InstallPath)) {
-                    mkdir -Force $InstallPath | Out-Null;
-                }
-                (New-Object System.Net.WebClient).DownloadFile($DotNetInstallerUri, "$InstallPath\dotnet-install.ps1");
-                & $InstallPath\dotnet-install.ps1 -Channel $DotNetChannel -Version $DotNetVersion -InstallDir $InstallPath;
+        # Get .NET Core CLI path if installed.
+        $FoundDotNetCliVersion = $null;
+        if (Get-Command dotnet -ErrorAction SilentlyContinue) {
+            $FoundDotNetCliVersion = dotnet --version;
+        }
 
-                Remove-PathVariable "$InstallPath"
-                $env:PATH = "$InstallPath;$env:PATH"
+        if ($FoundDotNetCliVersion -ne $DotNetVersion) {
+            $InstallPath = Join-Path $PSScriptRoot ".dotnet"
+            if (!(Test-Path $InstallPath)) {
+                mkdir -Force $InstallPath | Out-Null;
             }
+            (New-Object System.Net.WebClient).DownloadFile($DotNetInstallerUri, "$InstallPath\dotnet-install.ps1");
+            & $InstallPath\dotnet-install.ps1 -Channel $DotNetChannel -Version $DotNetVersion -InstallDir $InstallPath;
 
-            $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
-            $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
+            Remove-PathVariable "$InstallPath"
+            $env:PATH = "$InstallPath;$env:PATH"
+        }
+
+        $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
+        $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
 
             
-            Write-Host "Download GitVersion to build directory"
-            Get-ChildItem . -Recurse
-            $dotnetArgs = @('tool', 'install', 'gitversion.tool', "--version 5.6.6", "--tool-path $root")
-            & dotnet @dotnetArgs
+        Write-Host "Download GitVersion to build directory"
+        $dotnetArgs = @('tool', 'install', 'gitversion.tool', "--version 5.6.6", "--tool-path $root")
+        & dotnet @dotnetArgs
 
-            $GitVersionTool = (Get-ChildItem $root -Recurse -Filter "dotnet-gitversion*").FullName
-            Write-Host $GitVersionTool
-            Write-Output "##teamcity[setParameter name='env.GitVersionTool' value='$GitVersionTool']"
-        }
+        $GitVersionTool = (Get-ChildItem $root -Recurse -Filter "dotnet-gitversion*").FullName
+        Write-Host $GitVersionTool
+        Write-Output "##teamcity[setParameter name='env.GitVersionTool' value='$GitVersionTool']"
+    }
 
-        'Test' {
-            Install-Module Pester -MaximumVersion 4.99 -SkipPublisherCheck -Force
-            Import-Module Pester
-            Invoke-Pester (Resolve-Path "$root\Tests") -OutputFile "$($env:Build_ArtifactStagingDirectory)\test.results.xml" -OutputFormat NUnitXml
-        }
+    'Test' {
+        Install-Module Pester -MaximumVersion 4.99 -SkipPublisherCheck -Force
+        Import-Module Pester
+        Invoke-Pester (Resolve-Path "$root\Tests") -OutputFile "$($env:Build_ArtifactStagingDirectory)\test.results.xml" -OutputFormat NUnitXml
+    }
 
-        'Build' {
+    'Build' {
 
-            If (Test-Path $root\Output) {
+        If (Test-Path $root\Output) {
                 
-                Remove-Item $root\Output -Recurse -Force
+            Remove-Item $root\Output -Recurse -Force
 
-            }
+        }
 
-            $null = New-item "$root\Output\ChocoCCM" -ItemType Directory
+        $null = New-item "$root\Output\ChocoCCM" -ItemType Directory
 
-            Copy-Item "$root\ChocoCCM.psd1" "$root\Output\ChocoCCM"
+        Copy-Item "$root\ChocoCCM.psd1" "$root\Output\ChocoCCM"
 
-            $string = @'
+        $string = @'
 if(-not (Test-Path $env:ChocolateyInstall\license\chocolatey.license.xml)){
     throw "A licensed version of Chocolatey For Business is required to import and use this module"
 }
 
 '@
 
-            $string | Add-Content "$root\Output\ChocoCCM\ChocoCCM.psm1"
+        $string | Add-Content "$root\Output\ChocoCCM\ChocoCCM.psm1"
 
-            Get-ChildItem -Path $root\Public\*.ps1 | Foreach-Object {
+        Get-ChildItem -Path $root\Public\*.ps1 | Foreach-Object {
 
-                Get-Content $_.FullName | Add-Content "$root\Output\ChocoCCM\ChocoCCM.psm1"
-
-            }
-
-            Write-Host $env:GitVersionTool
+            Get-Content $_.FullName | Add-Content "$root\Output\ChocoCCM\ChocoCCM.psm1"
 
         }
 
-        'BuildLocal' {
-            If (Test-Path $root\Output) {
+        Write-Host $env:GitVersionTool
+
+    }
+
+    'BuildLocal' {
+        If (Test-Path $root\Output) {
                 
-                Remove-Item $root\Output -Recurse -Force
+            Remove-Item $root\Output -Recurse -Force
 
-            }
+        }
 
-            $null = New-item "$root\Output\ChocoCCM" -ItemType Directory
+        $null = New-item "$root\Output\ChocoCCM" -ItemType Directory
 
-            Copy-Item "$root\ChocoCCM.psd1" "$root\Output\ChocoCCM"
+        Copy-Item "$root\ChocoCCM.psd1" "$root\Output\ChocoCCM"
 
-            $string = @'
+        $string = @'
 if(-not $IsMacOS){
     if(-not (Test-Path $env:ChocolateyInstall\license\chocolatey.license.xml)){
         throw "A licensed version of Chocolatey For Business is required to import and use this module"
@@ -118,61 +123,61 @@ if(-not $IsMacOS){
 
 '@
 
-            $string | Add-Content "$root\Output\ChocoCCM\ChocoCCM.psm1"
+        $string | Add-Content "$root\Output\ChocoCCM\ChocoCCM.psm1"
 
-            Get-ChildItem -Path $root\Public\*.ps1 | Foreach-Object {
+        Get-ChildItem -Path $root\Public\*.ps1 | Foreach-Object {
 
-                Get-Content $_.FullName | Add-Content "$root\Output\ChocoCCM\ChocoCCM.psm1"
+            Get-Content $_.FullName | Add-Content "$root\Output\ChocoCCM\ChocoCCM.psm1"
 
-            }
+        }
+    }
+
+    'TestDeploy' {
+
+        if (Test-Path $root\Artifact) {
+            Remove-Item $root\Artifact -Recurse -Force
         }
 
-        'TestDeploy' {
+        New-Item $root\Artifact -ItemType Directory
 
-            if (Test-Path $root\Artifact) {
-                Remove-Item $root\Artifact -Recurse -Force
+        if ('ChocoCCM' -notin (Get-PSRepository).Name) {
+            $testRepo = @{
+                Name               = 'ChocoCCM'
+                PublishLocation    = "$env:RepoUrl"
+                SourceLocation     = "$env:RepoUrl"
+                InstallationPolicy = 'Trusted'
             }
 
-            New-Item $root\Artifact -ItemType Directory
-
-            if ('ChocoCCM' -notin (Get-PSRepository).Name) {
-                $testRepo = @{
-                    Name               = 'ChocoCCM'
-                    PublishLocation    = "$env:RepoUrl"
-                    SourceLocation     = "$env:RepoUrl"
-                    InstallationPolicy = 'Trusted'
-                }
-
-                Register-PSRepository @testRepo
-
-            }
-
-            $psdFile = Resolve-Path "$root\Output\ChocoCCM"
-            $publishParams = @{
-                Path        = $psdFile
-                Repository  = 'ChocoCCM'
-                NugetApiKey = '$env:NugetApiKey'
-            }
-
-            Publish-Module @publishParams
+            Register-PSRepository @testRepo
 
         }
 
-        'Deploy' {
-
-            $psdFile = Resolve-Path "$root\Output\ChocoCCM"
-            
-            $publishParams = @{
-                Path        = $psdFile
-                NugetApiKey = $env:NugetApiKey
-            }
-
-            Publish-Module @publishParams
+        $psdFile = Resolve-Path "$root\Output\ChocoCCM"
+        $publishParams = @{
+            Path        = $psdFile
+            Repository  = 'ChocoCCM'
+            NugetApiKey = '$env:NugetApiKey'
         }
 
-        default {
-            write-output "bob was here"
-        }
+        Publish-Module @publishParams
 
     }
+
+    'Deploy' {
+
+        $psdFile = Resolve-Path "$root\Output\ChocoCCM"
+            
+        $publishParams = @{
+            Path        = $psdFile
+            NugetApiKey = $env:NugetApiKey
+        }
+
+        Publish-Module @publishParams
+    }
+
+    default {
+        write-output "bob was here"
+    }
+
+}
 
