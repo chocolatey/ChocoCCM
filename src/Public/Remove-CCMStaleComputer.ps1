5c7ed19f8b2233b52f3ab0377a3da7e00e6bed4c
Stephen Valdinger <stephen@chocolatey.io>
2021-07-26 15:32:45 -0400

    (#48) Begin work of removing stale computers from CCM This commit lays the ground work for a mechanism to remove stale machines from the CCM database While this is scaffolded out, design decisions may yet need to be made.

---
+++ src/Public/Remove-CCMStaleComputer.ps1
@@ -0,0 +1,66 @@
+function Remove-CCMStaleComputer {
+    <#
+    .SYNOPSIS
+    Deletes stale computers from the CCM database.
+    
+    .DESCRIPTION
+    Deletes stale computers from the CCM database.
+    
+    .PARAMETER ComputerId
+    The id of the computer to remove.
+    
+    .PARAMETER Force
+    Don't prompt for confirmation.
+    
+    .EXAMPLE
+    Remove-CCMStaleComputer -ComputerId 1
+
+    .EXAMPLE
+    Get-CCMStaleComputer -StaleAge 30 | Remove-CCMStaleComputer -Force
+    
+    .NOTES
+    
+    #>
+    [cmdletBinding(SupportsShouldProcess)]
+    Param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [String[]]
+        $ComputerId,
+
+        [Parameter()]
+        [Switch]
+        $Force
+    )
+
+    process {
+
+        $ComputerId | ForEach-Object {
+            if ($Force -and -not $Confirm) {
+                $ConfirmPreference = 'None'
+                if ($PSCmdlet.ShouldProcess("$_", "Remove computer")) {
+                    $irmParams = @{
+                        Uri = "$($protocol)://$hostname/api/services/app/Computers/Delete?id=$_"
+                        Method = "DELETE"
+                        ContentType = "application/json"
+                        WebSession = $Session
+                    }
+
+                    Invoke-RestMethod @irmParams
+            
+                }
+            }
+            else {
+                if ($PSCmdlet.ShouldProcess("$_", "Remove computer")) {
+                    $irmParams = @{
+                        Uri = "$($protocol)://$hostname/api/services/app/Computers/Delete?id=$_"
+                        Method = "DELETE"
+                        ContentType = "application/json"
+                        WebSession = $Session
+                    }
+
+                    Invoke-RestMethod @irmParams
+                }
+            }
+        }
+    }
+}
