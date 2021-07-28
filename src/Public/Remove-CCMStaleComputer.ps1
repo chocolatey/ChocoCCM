ba0469319c625c60d13d37d72011d682f856ade3
Stephen Valdinger <stephen@chocolatey.io>
2021-07-28 14:17:10 -0400

    (#48) Add ability to remove by computername By adding this commit we can now call remove on an individual computer or array of computers by name. This requires a lookup of the id of the machine for the REST call for removal, but nothing terribly expensive.

--- src/Public/Remove-CCMStaleComputer.ps1
+++ src/Public/Remove-CCMStaleComputer.ps1
@@ -8,6 +8,9 @@ function Remove-CCMStaleComputer {
     
     .PARAMETER ComputerId
     The id of the computer to remove.
+
+    .PARAMETER ComputerName
+    The name of the computer to remove.
     
     .PARAMETER Force
     Don't prompt for confirmation.
@@ -21,12 +24,16 @@ function Remove-CCMStaleComputer {
     .NOTES
     
     #>
-    [cmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+    [cmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High', DefaultParameterSetName = "Id")]
     Param(
         [Alias('Id')]
-        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, ParameterSetName = "Id")]
         [Int[]]
         $ComputerId,
+
+        [Parameter(Mandatory, ParameterSetName = "Name")
+        [String[]]
+        $ComputerName,
 
         [Parameter()]
         [Switch]
@@ -35,38 +42,83 @@ function Remove-CCMStaleComputer {
 
     process {
 
-        $ComputerId | ForEach-Object {
-            if ($Force -and -not $Confirm) {
-                $ConfirmPreference = 'None'
-                if ($PSCmdlet.ShouldProcess("$_", "Remove computer")) {
-                    $irmParams = @{
-                        Uri = "$($protocol)://$hostname/api/services/app/Computers/Delete?id=$_"
-                        Method = "DELETE"
-                        ContentType = "application/json"
-                        WebSession = $Session
-                    }
+        switch ($PSCmdlet.ParameterSetName) {
 
-                    $null = Try { 
-                        Invoke-RestMethod @irmParams -ErrorAction Stop
-                    } catch { $_.Exception.Message}
-            
+            'Id' {
+                $ComputerId | ForEach-Object {
+                    if ($Force -and -not $Confirm) {
+                        $ConfirmPreference = 'None'
+                        if ($PSCmdlet.ShouldProcess("$_", "Remove computer")) {
+                            $irmParams = @{
+                                Uri         = "$($protocol)://$hostname/api/services/app/Computers/Delete?id=$_"
+                                Method      = "DELETE"
+                                ContentType = "application/json"
+                                WebSession  = $Session
+                            }
+        
+                            $null = Try { 
+                                Invoke-RestMethod @irmParams -ErrorAction Stop
+                            }
+                            catch { $_.Exception.Message }
+                    
+                        }
+                    }
+                    else {
+                        if ($PSCmdlet.ShouldProcess("$_", "Remove computer")) {
+                            $irmParams = @{
+                                Uri         = "$($protocol)://$hostname/api/services/app/Computers/Delete?id=$_"
+                                Method      = "DELETE"
+                                ContentType = "application/json"
+                                WebSession  = $Session
+                            }
+        
+                            $null = try {
+                                Invoke-RestMethod @irmParams -ErrorAction Stop
+                            }
+                            catch { $_.Exception.Message }
+                        }
+                    }
                 }
             }
-            else {
-                if ($PSCmdlet.ShouldProcess("$_", "Remove computer")) {
-                    $irmParams = @{
-                        Uri = "$($protocol)://$hostname/api/services/app/Computers/Delete?id=$_"
-                        Method = "DELETE"
-                        ContentType = "application/json"
-                        WebSession = $Session
+        
+            'Name' {
+                $ComputerName | ForEach-Object {
+                    $id = (Get-CCMComputer -Computer $_).id
+                    if ($Force -and -not $Confirm) {
+                        $ConfirmPreference = 'None'
+                        if ($PSCmdlet.ShouldProcess("$id", "Remove computer")) {
+                            $irmParams = @{
+                                Uri         = "$($protocol)://$hostname/api/services/app/Computers/Delete?id=$id"
+                                Method      = "DELETE"
+                                ContentType = "application/json"
+                                WebSession  = $Session
+                            }
+    
+                            $null = Try { 
+                                Invoke-RestMethod @irmParams -ErrorAction Stop
+                            }
+                            catch { $_.Exception.Message }
+                
+                        }
                     }
-
-                    $null = try {
-                        Invoke-RestMethod @irmParams -ErrorAction Stop
+                    else {
+                        if ($PSCmdlet.ShouldProcess("$id", "Remove computer")) {
+                            $irmParams = @{
+                                Uri         = "$($protocol)://$hostname/api/services/app/Computers/Delete?id=$id"
+                                Method      = "DELETE"
+                                ContentType = "application/json"
+                                WebSession  = $Session
+                            }
+    
+                            $null = try {
+                                Invoke-RestMethod @irmParams -ErrorAction Stop
+                            }
+                            catch { $_.Exception.Message }
+                        }
                     }
-                    catch { $_.Exception.Message}
                 }
             }
         }
     }
+        
 }
