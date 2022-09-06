f99a92967de36907b65ffe2ac242b644292155a1
Rain Sallow <32407840+vexx32@users.noreply.github.com>
2022-09-06 15:19:09 -0400

    (#69) Remove faulty license check for now
    This check was only checking for the presence of a file and mostly
    caused issues in the build process on our build servers.
    
    Will file a follow up issue to construct a more appropriate check and
    sort out our build agents at that time.

--- src/module.psm1
+++
@@ -1,5 +0,0 @@
-if (-not $IsMacOS -and -not $IsLinux) {
-    if (-not (Test-Path $env:ChocolateyInstall\license\chocolatey.license.xml)) {
-        throw "A licensed version of Chocolatey For Business is required to import and use this module"
-    }
-}
