2b62e0104848b2d6c64267273de2feb3554dd9b7
James Ruskin <github@justpowerdown.com>
2023-05-06 17:15:20 +0100

    (#66) Fixes Remove-CCMStaleDeployment
    Removes the -All parameter from Get-CCMDeployment, which means this might work.
    
    Adjusts the test to be a little better.

--- src/Tests/Public/Remove-CCMStaleDeployment.Tests.ps1
+++ src/Tests/Public/Remove-CCMStaleDeployment.Tests.ps1
@@ -8,23 +8,32 @@ Describe "Remove-CCMStaleDeployment" {
             $script:Protocol = "http"
         }
         Mock Invoke-RestMethod -ModuleName ChocoCCM
+        Mock Get-Date -ModuleName ChocoCCM {
+            $script:TestDate
+        }
         Mock Get-CCMDeployment -ModuleName ChocoCCM {
-            1..60 | ForEach-Object {
-                @{
-                    Deployment = $_
-                    CreationDate = (Get-Date).AddDays(-$_)
-                    Result = 0  # Is 0 really a bad state?
-                }
-            }
+            $script:TestData
         }
         Mock Remove-CCMDeployment -ModuleName ChocoCCM
     }
 
-    # This function is broken. It tries to use the -All parameter of Get-CCMDeployment, but that parameter doesn't exist.
-    Context "Removing Stale Deployments" -Skip {
+    Context "Removing Stale Deployments" {
         BeforeAll {
             $TestParams = @{
                 Age = "30"
+            }
+
+            $script:TestData = 1..100 | ForEach-Object {
+                @{
+                    Deployment   = $_
+                    CreationDate = (Get-Date).AddDays((Get-Random -Minimum (- $TestParams.Age * 2) -Maximum 0))
+                    Result       = 0, 1, 2, 8 | Get-Random
+                }
+            }
+            $script:TestDate = Get-Date
+            $StaleDeployments = $script:TestData.Where{
+                $_.CreationDate -ge $script:TestDate.AddDays(-$TestParams.Age) -and
+                $_.Result -ne 1
             }
 
             $Result = Remove-CCMStaleDeployment @TestParams -Confirm:$false
@@ -34,10 +43,8 @@ Describe "Remove-CCMStaleDeployment" {
             Should -Invoke Get-CCMDeployment -ModuleName ChocoCCM -Scope Context -Times 1 -Exactly
         }
 
-        It "Calls Remove-CCMDeployment to remove deployments with age greater than 30 days" {
-            Should -Invoke Remove-CCMDeployment -ModuleName ChocoCCM -Scope Context -Times 30 -Exactly -ParameterFilter {
-                $Deployment -ge 30  # This test could be improved, but as the function is broken it is what it is.
-            }
+        It "Calls Remove-CCMDeployment to remove stale deployments from the last $($TestParams.Age) days" {
+            Should -Invoke Remove-CCMDeployment -ModuleName ChocoCCM -Scope Context -Times $StaleDeployments.Count -Exactly
         }
 
         It "Returns Nothing" {
